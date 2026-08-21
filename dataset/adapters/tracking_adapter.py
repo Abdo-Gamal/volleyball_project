b2334df5205f8b6b834ea5025e18c56ed869a7e8
abdulrahman <bgmal917@gmail.com>
2026-08-21 23:45:40 +0300

    add cache to track adaptor and refactor it

--- dataset/adapters/tracking_adapter.py
+++ dataset/adapters/tracking_adapter.py
@@ -12,22 +12,27 @@ class TrackingAdapter(BaseAdapter):
     def load_sample(self, idx: int):
         sample = self.raw[idx]
         
-        frames_list = sample["frames"][5:14]
-        boxes_list = sample["boxes"][5:14]
-        cropped_frames = []
+        if idx in self._cache:
+            clip_tensor = self._cache[idx]
+        else:
+            frames_list = sample["frames"][5:14]
+            boxes_list = sample["boxes"][5:14]
+            cropped_frames = []
+            
+            for img_path, box in zip(frames_list, boxes_list):
+                img = cv2.imread(img_path)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                
+                x1, y1, x2, y2 = map(int, box)
+                crop = img[y1:y2, x1:x2]
+                crop = cv2.resize(crop, (224, 224))
+                
+                crop_tensor = torch.from_numpy(crop).permute(2, 0, 1)
+                cropped_frames.append(crop_tensor)
+                
+            clip_tensor = torch.stack(cropped_frames)
+            self._cache[idx] = clip_tensor
         
-        for img_path, box in zip(frames_list, boxes_list):
-            img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            
-            x1, y1, x2, y2 = map(int, box)
-            crop = img[y1:y2, x1:x2]
-            crop = cv2.resize(crop, (224, 224))
-            
-            crop_tensor = torch.from_numpy(crop).permute(2, 0, 1)
-            cropped_frames.append(crop_tensor)
-            
-        clip_tensor = torch.stack(cropped_frames)
         label_int = self.label_map[sample["label"]]
         
         return clip_tensor, label_int
