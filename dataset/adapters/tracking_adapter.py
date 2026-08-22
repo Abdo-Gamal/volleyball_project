8511191e3989bca8f31f806480ac603a92d65152
abdulrahman <bgmal917@gmail.com>
2026-08-22 13:07:29 +0300

    use read indx return from bulid index in cache

--- dataset/adapters/tracking_adapter.py
+++ dataset/adapters/tracking_adapter.py
@@ -2,6 +2,10 @@ import cv2
 import torch
 from dataset.adapters.base_adapter import BaseAdapter
 
+import cv2
+import torch
+from dataset.adapters.base_adapter import BaseAdapter
+
 class TrackingAdapter(BaseAdapter):
     """
     Adapter for tracking datasets.
@@ -14,15 +18,15 @@ class TrackingAdapter(BaseAdapter):
         real_idx = self._index[idx]
         sample = self.raw[real_idx]
         
-        if idx in self._cache:
-            clip_tensor = self._cache[idx]
+        if real_idx in self._cache:
+            clip_tensor = self._cache[real_idx]
         else:
             frames_list = sample["frames"][5:14]
             boxes_list = sample["boxes"][5:14]
             cropped_frames = []
             
             for img_path, box in zip(frames_list, boxes_list):
-                img = cv2.imread(img_path)
+                img = cv2.imread(img_path)         
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 
                 x1, y1, x2, y2 = map(int, box)
@@ -33,7 +37,8 @@ class TrackingAdapter(BaseAdapter):
                 cropped_frames.append(crop_tensor)
                 
             clip_tensor = torch.stack(cropped_frames)
-            self._cache[idx] = clip_tensor
+            
+            self._cache[real_idx] = clip_tensor
         
         label_int = self.label_map[sample["label"]]
         
